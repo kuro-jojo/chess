a0c21e79d27e482c939034407c80374f3467d7da
Jonathan <kurojojo08@gmail.com>
2023-03-26 15:24:07 +0200

    add chess board and pieces

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+  
+const server = app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
