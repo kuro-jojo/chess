6725ccaa627f519d713dca1735092e9ef66fb5dd
Jonathan <kurojojo08@gmail.com>
2023-04-05 01:33:27 +0200

    change views->public

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const io = require('socket.io')(server, {
 });
 
 // Serve static files from the "public" directory
-app.use(express.static('src/views'));
+app.use(express.static('src/public'));
 
 
-app.get('/socket.io', (req, res) => {
-    req.headers['access-control-allow-origin'] = 'htttp://192.168.196.124:3000'
-});
+// app.get('/socket.io', (req, res) => {
+//     req.headers['access-control-allow-origin'] = 'htttp://192.168.196.124:3000'
+// });
 
 
 io.on('connection', (socket) => {
@@ -33,19 +33,3 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => console.log('listening on http://localhost:3000'));
-
-// const connect = require('connect');
-// const serveStatic = require('serve-static');
-// const mime = require('mime');
-// const app = connect();
-
-// app.use(serveStatic('src/views'));
-
-// // Add the following middleware to set the correct MIME type for JavaScript files
-// app.use((req, res, next) => {
-//     if (req.url.endsWith('.js')) {
-//         res.setHeader('Content-Type', mime.getType('js'));
-//     }
-//     next();
-// });
-// app.listen(3000);
--- src/views/js/consts.js
+++
@@ -1,9 +0,0 @@
-export const PAWN = "p";
-export const KNIGHT = "n";
-export const BISHOP = "b";
-export const ROOK = "r";
-export const QUEEN = "q";
-export const KING = "k";
-
-export const WHITE_PIECE = "w";
-export const BLACK_PIECE = "b";
